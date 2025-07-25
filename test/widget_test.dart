5bf5bf208eb7666fea64bb2e0b07323a8e905605
neakuirman <neakuirman@gmail.com>
2025-07-25 18:20:56 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:notez/main.dart'; // Pastikan ini sesuai dengan lokasi main.dart kamu
+
 void main() {
-  testWidgets('Example widget test', (WidgetTester tester) async {
-    // TODO: Add widget test
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that a widget with expected text appears.
+    expect(find.text('This Aplication is under development'), findsOneWidget); // Ganti sesuai isi aplikasi kamu
   });
 }
