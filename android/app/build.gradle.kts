394e9f5dec1d17e785a4a381c52d9966814f2d6a
neakuirman <neakuirman@gmail.com>
2025-07-25 16:45:37 +0800

    Upload ulang proyek Flutter yang sudah rapi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.notez_app"
+    namespace = "com.example.notez"
     compileSdk = flutter.compileSdkVersion
-// ndkVersion = flutter.ndkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.notez_app"
+        applicationId = "com.example.notez"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
